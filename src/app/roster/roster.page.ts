d230dfc0a80fc82893d08da1a2f90bba6f54af99
nitrotap <9121718+nitrotap@users.noreply.github.com>
2023-03-09 20:51:25 -0700

    adding toast on delete

--- src/app/roster/roster.page.ts
+++ src/app/roster/roster.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Student, StudentsService } from '../../students.service';
-import { ActionSheetController, AlertController } from '@ionic/angular';
+import { ActionSheetController, AlertController, ToastController } from '@ionic/angular';
 
 
 @Component({
@@ -14,12 +14,20 @@ export class RosterPage implements OnInit {
   constructor(
     private actionSheetController: ActionSheetController,
     private alertController: AlertController,
-    private studentService: StudentsService
+    private studentService: StudentsService,
+    private toastController: ToastController
   ) 
   { }
 
   async deleteStudent(student: Student) {
     this.students = this.students.filter(x => x.id !== student.id);
+    const alert = await this.toastController.create({
+      message: `${student.firstName} ${student.lastName} has been deleted.`,
+      position: 'top',
+      duration: 2000
+    });
+
+    await alert.present();  
   }
 
   async presentDeleteAlert(student: Student) {
